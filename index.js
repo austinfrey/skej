56eec3a40cd61a0459aa2e7f183313c747d27ffa
Austin Frey <aafrey85@gmail.com>
2017-09-25 15:55:39 -0400

    Removed unnecessary dependencies
    Signed-off-by: Austin Frey <aafrey85@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-'use strict'
+'use strict';
 
-module.exports = require('./skej')
+module.exports = require('./skej');
--- skej/index.js
+++ skej/index.js
@@ -3,8 +3,8 @@
 const FaaS = require('openfaas');
 const Redular = require('redular');
 
-const skej = (schedule) => {
-	const {oneOff, recurring, pipe} = schedule
+const skej = schedule => {
+	const {oneOff, recurring, pipe} = schedule;
 
 	const options = {
 		autoconfig: true,
@@ -17,29 +17,27 @@ const skej = (schedule) => {
 	const faas = FaaS('http://localhost:8080');
 	const redular = new Redular(options);
 
-	const scheduleOneOff = (oneOffList) => {
-		const date = new Date()
+	const scheduleOneOff = oneOffList => {
+		const date = new Date();
 
 		for (var func in oneOffList) {
-
 			redular.defineHandler(oneOffList[func].key, () => {
 				faas.invoke(oneOffList[func].key)
 					.then(x => console.log(x.body))
-					.catch(err => console.log(err))
-			})
+					.catch(err => console.log(err));
+			});
 
 			redular.scheduleEvent(
 				oneOffList[func].key,
 				date.setSeconds(date.getSeconds() + oneOffList[func].initialRun)
-			)
+			);
 		}
-	}
+	};
 
-	const scheduleRecurring = (recurringList) => {
-		const date = new Date()
+	const scheduleRecurring = recurringList => {
+		const date = new Date();
 
 		for (var func in recurringList) {
-
 			redular.defineHandler(recurringList[func].key, () => {
 				faas.invoke(recurringList[func].key, recurringList[func].data)
 					.then(x => console.log(x.body))
@@ -47,20 +45,19 @@ const skej = (schedule) => {
 						date.setSeconds(date.getSeconds() + recurringList[func].recurring);
 						redular.scheduleEvent(recurringList[func].key, date);
 					})
-					.catch(err => console.log(err))
-			})
+					.catch(err => console.log(err));
+			});
 
 			redular.scheduleEvent(
 				recurringList[func].key,
 				date.setSeconds(date.getSeconds() + recurringList[func].initialRun)
-			)
+			);
 		}
-	}
-	scheduleOneOff(oneOff)
-	scheduleRecurring(recurring)
+	};
+	scheduleOneOff(oneOff);
+	scheduleRecurring(recurring);
 
 	console.log('starting');
+};
 
-}
-
-module.exports = skej
+module.exports = skej;
